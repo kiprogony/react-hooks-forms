ca7b94f01fdb1dec4bd6648c1c35f1659a2e0700
Daryl Rogony <darylrogony@gmail.com>
2022-05-14 23:57:26 +0300

    Completed Assignment fit: controlling form values using state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
-
+function Form(props) {
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
-      <button type="submit">Submit</button>
+      <input 
+        type="text"
+        onChange={props.handleFirstNameChange}
+        value={props.firstName}
+      />
+      <input 
+        type="text"
+        onChange={props.handleLastNameChange}
+        value={props.lastName}
+      />
+      <button type="submit">submit</button>
     </form>
   );
 }
